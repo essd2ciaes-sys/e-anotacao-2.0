4a839844b11407aced00b5785875e32ef8319b40
essd2ciaes-sys <essd2ciaes@gmail.com>
2026-05-19 14:13:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,19 +2,19 @@
 const CONFIG = {
 
     API_1CIA:
-    "URL_APPS_SCRIPT_1CIA",
+    "https://script.google.com/macros/s/AKfycbyaClZRqr4vxLC1cbTZoCDOG6navwmWcHm-svcDYOg57ZBqPQvHUXh7nTvb-ip_rZgI/exec",
 
     API_2CIA:
-    "URL_APPS_SCRIPT_2CIA",
+    "https://script.google.com/macros/s/AKfycbwVrGlWajoZx-frBxJJspayB1JgoeXTTor-FxYWRhyo8G2HA0YnXxunA-ziN3Y8ziSm/exec",
 
     API_3CIA:
-    "URL_APPS_SCRIPT_3CIA",
+    "https://script.google.com/macros/s/AKfycbx0hrI1_zC5fxb8-mjzrImo6W63Bnu3sr3YARz7ikmku3SkAj9R0_j-fOH2ykmxbAEE/exec",
 
     API_4CIA:
-    "URL_APPS_SCRIPT_4CIA",
+    "https://script.google.com/macros/s/AKfycbzPJFN90D6tfNw-ENIt4VvDmfzixiA_zscN-2gAJ3NN_Nwj3Jj51y0PxLyILhAkxgaU/exec",
 
     API_5CIA:
-    "URL_APPS_SCRIPT_5CIA",
+    "https://script.google.com/macros/s/AKfycbxHciBVFEP-pYbew_qJ3eK8Q5t5HwaqCgzVYiqbuybgLJj-riJ26edaJ7Lro3XBFNr4-Q/exec",
 
     TOKEN:
     "ESSD2026SEGURANCA"
