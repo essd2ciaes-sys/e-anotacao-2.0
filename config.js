48a0b94ed30fb5944e85d3f197364498aa219233
essd2ciaes-sys <essd2ciaes@gmail.com>
2026-05-19 11:46:52 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+
+const CONFIG = {
+
+    API_1CIA:
+    "URL_APPS_SCRIPT_1CIA",
+
+    API_2CIA:
+    "URL_APPS_SCRIPT_2CIA",
+
+    API_3CIA:
+    "URL_APPS_SCRIPT_3CIA",
+
+    API_4CIA:
+    "URL_APPS_SCRIPT_4CIA",
+
+    API_5CIA:
+    "URL_APPS_SCRIPT_5CIA",
+
+    TOKEN:
+    "ESSD2026SEGURANCA"
+
+};
+
